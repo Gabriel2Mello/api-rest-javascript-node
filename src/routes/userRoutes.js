189573bd0475cf80178b9672fb95a08d020349e0
Gabriel Mello <gabriel.mellosjb@gmail.com>
2023-06-13 14:14:44 -0300

    Bearer token

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import userController from '../controllers/UserController';
+import loginRequired from '../middlewares/loginRequired';
 
 const router = new Router;
 
 router.post('/', userController.create);
-router.get('/', userController.getAll);
+router.get('/', loginRequired, userController.getAll);
 router.get('/:id', userController.get);
 router.put('/:id', userController.update);
 router.delete('/:id', userController.delete);
